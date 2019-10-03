a78713abed95a4f6a7a246d79838547cd8bc9167
Alex <lisandra.nazarova@gmail.com>
2019-10-03 11:43:18 +0300

    rewrite async/await

--- controllers/todo-list.js
+++ controllers/todo-list.js
@@ -3,25 +3,24 @@ const io = require('../socket');
 
 const Task = require('../models/task');
 
-exports.getTasks = (req, res, next) => {
-  Task.find().sort({createdAt: -1})
-    .then(tasks => {
-      res
+exports.getTasks = async (req, res, next) => {
+  try {
+    const tasks = await Task.find().sort({createdAt: -1});
+    res
         .status(200)
         .json({
           message: 'Fetched tasks successfully.',
           tasks: tasks,
         });
-    })
-    .catch(err => {
+  } catch (err) {
       console.error(err.message);
       res
-        .status(500)
-        .json({ errors: [{ msg: 'Server error.' }] });
-    });
+          .status(500)
+          .json({ errors: [{ msg: 'Server error.' }] });
+  }
 };
 
-exports.createTask = (req, res, next) => {
+exports.createTask = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res
@@ -29,105 +28,96 @@ exports.createTask = (req, res, next) => {
         .json({ errors: [{ msg: 'Validation failed, entered data is incorrect.' }] });
   }
   const title = req.body.title;
-  const task = new Task({
-    title: title
-  });
-  task
-    .save()
-    .then(result => {
-      io.getIO().emit('task', {
-        action: 'create',
-        task: result
-      });
-      res.status(201).json({
-        message: 'Task created successfully!',
-        task: result
-      });
-    })
-    .catch(err => {
+  try {
+    const task = new Task({
+      title: title
+    });
+    await task.save();
+    io.getIO().emit('task', {
+      action: 'create',
+      task: task
+    });
+    res.status(201).json({
+      message: 'Task created successfully!',
+      task: task
+    });
+  } catch (err) {
       console.error(err.message);
       res
-        .status(500)
-        .json({ errors: [{ msg: 'Server error.' }] });
-    });
+          .status(500)
+          .json({ errors: [{ msg: 'Server error.' }] });
+  }
 };
 
-exports.getTask = (req, res, next) => {
-  const taskId = req.params.taskId;
-  Task.findById(taskId)
-    .then(task => {
-      if (!task) {
-        return res
-          .status(404)
-          .json({ errors: [{ msg: 'Could not find task.' }] });
-      }
-      res.status(200).json({ message: 'Task fetched.', task: task });
-    })
-    .catch(err => {
+exports.getTask = async (req, res, next) => {
+  try {
+    const taskId = req.params.taskId;
+    const task = await Task.findById(taskId);
+    if (!task) {
+      return res
+        .status(404)
+        .json({ errors: [{ msg: 'Could not find task.' }] });
+    }
+    res.status(200).json({ message: 'Task fetched.', task: task });
+  } catch (err) {
       console.error(err.message);
       res
-        .status(500)
-        .json({ errors: [{ msg: 'Server error.' }] });
-    });
+          .status(500)
+          .json({ errors: [{ msg: 'Server error.' }] });
+  }
 };
 
-exports.updateTask = (req, res, next) => {
-  const taskId = req.params.taskId;
+exports.updateTask = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res
         .status(422)
         .json({ errors: [{ msg: 'Validation failed, entered data is incorrect.' }] });
   }
-  const {title, completed} = req.body;
-  Task.findById(taskId)
-    .then(task => {
-      if (!task) {
-        return res
-          .status(404)
-          .json({ errors: [{ msg: 'Could not find task.' }] });
-      }
-      if (title) task.title = title;
-      if (typeof completed !== "undefined") task.completed = completed;
-      return task.save();
-    })
-    .then(result => {
-      io.getIO().emit('task', {
-        action: 'update',
-        task: result
-      });
-      res.status(200).json({ message: 'Task updated!', task: result });
-    })
-    .catch(err => {
+  try {
+    const taskId = req.params.taskId;
+    const {title, completed} = req.body;
+    const task = await Task.findById(taskId);
+    if (!task) {
+      return res
+        .status(404)
+        .json({ errors: [{ msg: 'Could not find task.' }] });
+    }
+    if (title) task.title = title;
+    if (typeof completed !== "undefined") task.completed = completed;
+    await task.save();
+    io.getIO().emit('task', {
+      action: 'update',
+      task: task
+    });
+    res.status(200).json({ message: 'Task updated!', task: task });
+  } catch (err) {
       console.error(err.message);
       res
-        .status(500)
-        .json({ errors: [{ msg: 'Server error.' }] });
-    });
+          .status(500)
+          .json({ errors: [{ msg: 'Server error.' }] });
+  }
 };
 
-exports.deleteTask = (req, res, next) => {
-  const taskId = req.params.taskId;
-  Task.findById(taskId)
-    .then(task => {
-      if (!task) {
-        return res
-          .status(404)
-          .json({ errors: [{ msg: 'Could not find task.' }] });
-      }
-      return Task.findByIdAndRemove(taskId);
-    })
-    .then(result => {
-      io.getIO().emit('task', {
-        action: 'delete',
-        task: taskId
-      });
-      res.status(200).json({ message: 'Deleted task.' });
-    })
-    .catch(err => {
+exports.deleteTask = async (req, res, next) => {
+  try {
+    const taskId = req.params.taskId;
+    const task = await Task.findById(taskId);
+    if (!task) {
+      return res
+        .status(404)
+        .json({ errors: [{ msg: 'Could not find task.' }] });
+    }
+    await Task.findByIdAndRemove(taskId);
+    io.getIO().emit('task', {
+      action: 'delete',
+      task: taskId
+    });
+    res.status(200).json({ message: 'Deleted task.' });
+  } catch (err) {
       console.error(err.message);
       res
-        .status(500)
-        .json({ errors: [{ msg: 'Server error.' }] });
-    });
+          .status(500)
+          .json({ errors: [{ msg: 'Server error.' }] });
+  }
 };
